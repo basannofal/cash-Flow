2331a14e2981b68be7e5098687c840f27c8599cc
Sunasarahusenahmad <sunasarahusenahmad07@gmail.com>
2023-09-08 16:54:15 +0530

    msidebar, sidebar, conn file change

--- component/Sidebar.js
+++ component/Sidebar.js
@@ -1,61 +1,89 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react'
-import { MdPeople } from 'react-icons/md'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { MdPeople } from "react-icons/md";
 
 const Sidebar = () => {
+  const router = useRouter();
 
-    const router = useRouter();
+  // logout code
+  const handleLogout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("is_login");
+    router.push("/login");
+  };
 
-    // logout code 
-    const handleLogout = (e) => {
-        e.preventDefault();
-        localStorage.removeItem("is_login");
-        router.push("/login")
-    }
+  // Add an array to store your navigation links
+  const navigationLinks = [
+    { href: "/", text: "Dashboard", icon: "bx bxs-dashboard" },
+    { href: "/category", text: "Category", icon: "bx bx-analyse" },
+    { href: "/memberlist", text: "Member List", icon: "bx bx-user" },
+    { href: "/user", text: "Users", icon: "bx bx-group" },
+  ];
 
-    useEffect(() => {
-        const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+  // Function to check if a link is active
+  const isLinkActive = (href) => {
+    return router.pathname === href;
+  };
 
-        sideLinks.forEach(item => {
-            const li = item.parentElement;
-            item.addEventListener('click', () => {
-                sideLinks.forEach(i => {
-                    i.parentElement.classList.remove('active');
-                })
-                li.classList.add('active');
-            })
+  useEffect(() => {
+    // Update the active link when the route changes
+    const sideLinks = document.querySelectorAll(
+      ".sidebar .side-menu li a:not(.logout)"
+    );
+
+    sideLinks.forEach((item) => {
+      const li = item.parentElement;
+      if (isLinkActive(item.getAttribute("href"))) {
+        li.classList.add("active");
+      }
+
+      item.addEventListener("click", () => {
+        sideLinks.forEach((i) => {
+          i.parentElement.classList.remove("active");
         });
-    }, []);
-    return (
-        <>
-            {/* Sidebar  */}
-            <div className="sidebar">
-                <Link href="/" className="logo">
-                    <i className='bx bxs-caret-up-circle'></i>
-                    <div className="logo-name"><span>Cash</span>Flow</div>
-                </Link>
-                <ul className="side-menu">
-                    <li className="active"><Link href='/'><i className='bx bxs-dashboard'></i>Dashboard</Link></li>
-                    <li><Link href='/category'><i className='bx bx-analyse'> </i>Category</Link></li>
-                     <li><Link href='/memberlist'> <MdPeople className='bx' />Member List</Link></li> 
-                    <li><Link href='/user'><i className='bx bx-group'></i>Users</Link></li>
-                    {/* <li><Link href='/payment'><i className='bx bx-message-square-dots'></i>Add Payment</Link></li>
-                    <li><Link href='/borrowmoney'><i className='bx bx-message-square-dots'></i>Borrow Payment</Link></li> */}
-                    <li><a><i className='bx bx-cog'></i>Settings</a></li>
-                </ul>
-                <ul className="side-menu">
-                    <li onClick={handleLogout}>
-                        <a href="#" className="logout">
-                            <i className='bx bx-log-out-circle'></i>
-                            Logout
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            {/* End of Sidebar  */}
-        </>
-    )
-}
+        li.classList.add("active");
+      });
+    });
+  }, [router.pathname]);
 
-export default Sidebar
+  return (
+    <>
+      {/* Sidebar */}
+      <div className="sidebar">
+        <Link href="/" className="logo">
+          <i className="bx bxs-caret-up-circle"></i>
+          <div className="logo-name">
+            <span>Cash</span>Flow
+          </div>
+        </Link>
+        <ul className="side-menu">
+          {navigationLinks.map((link, index) => (
+            <li key={index} className={isLinkActive(link.href) ? "active" : ""}>
+              <Link href={link.href}>
+                <i className={link.icon}></i>
+                {link.text}
+              </Link>
+            </li>
+          ))}
+          <li>
+            <a>
+              <i className="bx bx-cog"></i>Settings
+            </a>
+          </li>
+        </ul>
+        <ul className="side-menu">
+          <li onClick={handleLogout}>
+            <a href="#" className="logout">
+              <i className="bx bx-log-out-circle"></i>
+              Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* End of Sidebar */}
+    </>
+  );
+};
+
+export default Sidebar;
