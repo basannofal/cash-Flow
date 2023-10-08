004fe3af3b020626ef024e90a497c5c2a986f56e
Sunasarahusenahmad <sunasarahusenahmad07@gmail.com>
2023-10-08 09:01:42 +0530

    08-10-2023 - updated commited

--- component/MemberDashboard/Dashboard.js
+++ component/MemberDashboard/Dashboard.js
@@ -1,43 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchMemberAsync, fetchPerMemberAsync } from "@/store/slices/MemberSlice";
+import {
+  fetchMemberAsync,
+  fetchPerMemberAsync,
+} from "@/store/slices/MemberSlice";
 import { totalborrowdepositeAsync } from "@/store/slices/MemberBorrowDepositeSlice";
 import { totalborrowpaymentAsync } from "@/store/slices/BorrowSlice";
 import { totalreturnpaymentAsync } from "@/store/slices/ReturnPaymentSlice";
 import { totalpaymentAsync } from "@/store/slices/PaymentSlice";
 import InitialsAvatar from "../utilities/InitialsAvatar";
 import { fetchAccountingDetailAsync } from "@/store/slices/Accounting";
+import Pagination from "../Pagination";
 
 const Dashboard = ({ memberId }) => {
-
-
-  const totalreturnpayment = useSelector((state) => state.returnpayment.totalreturnpayment);
+  const totalreturnpayment = useSelector(
+    (state) => state.returnpayment.totalreturnpayment
+  );
   const totalpayment = useSelector((state) => state.payment.totalpayment);
   const account = useSelector((state) => state.account.account);
 
-
-  let totalborrowdeposite = useSelector((state) => state.borrowdeposite.totalborrowdepositepayment);
+  let totalborrowdeposite = useSelector(
+    (state) => state.borrowdeposite.totalborrowdepositepayment
+  );
   let totalborrow = useSelector((state) => state.borrow.totalborrowpayment);
-
-
-
 
   const dispatch = useDispatch();
   const [selectedCategory, setSelectedCategory] = useState(0);
-  const [isFilterVisible, setIsFilterVisible] = useState(false); // State for filter 
+  const [isFilterVisible, setIsFilterVisible] = useState(false); // State for filter
   const [totalCredit, setTotalCredit] = useState(0);
   const [totalDebit, setTotalDebit] = useState(0);
   const [NetProfit, setNetProfit] = useState(0);
-
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 10;
 
   useEffect(() => {
     const fetchData = async () => {
       dispatch(fetchPerMemberAsync(memberId));
       dispatch(totalborrowdepositeAsync(memberId));
-      dispatch(totalborrowpaymentAsync(memberId))
+      dispatch(totalborrowpaymentAsync(memberId));
       dispatch(totalreturnpaymentAsync(memberId));
-      dispatch(totalpaymentAsync(memberId))
-      dispatch(fetchAccountingDetailAsync(memberId))
+      dispatch(totalpaymentAsync(memberId));
+      dispatch(fetchAccountingDetailAsync(memberId));
     };
     fetchData();
   }, []);
@@ -46,11 +49,9 @@ const Dashboard = ({ memberId }) => {
     setSelectedCategory(category);
   };
 
-
   const handleFilterIconClick = () => {
     setIsFilterVisible(!isFilterVisible); // Toggle the filter visibility
   };
-
 
   // Filtered data based on selected category
   const filteredAccount = account.filter((e) => {
@@ -67,8 +68,6 @@ const Dashboard = ({ memberId }) => {
     }
   });
 
-
-
   useEffect(() => {
     // Calculate total credit and total debit here
     let creditTotal = 0;
@@ -84,10 +83,16 @@ const Dashboard = ({ memberId }) => {
 
     setTotalCredit(creditTotal);
     setTotalDebit(debitTotal);
-    setNetProfit(creditTotal  - debitTotal)
+    setNetProfit(creditTotal - debitTotal);
   }, [filteredAccount]); // Update totals when filteredAccount changes
 
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedData = filteredAccount.slice(startIndex, endIndex);
 
   return (
     <>
@@ -153,7 +158,6 @@ const Dashboard = ({ memberId }) => {
           </div>
           <table>
             <thead>
-
               <tr>
                 <th>Transaction ID</th>
                 <th>Payment Type</th>
@@ -166,36 +170,31 @@ const Dashboard = ({ memberId }) => {
               </tr>
             </thead>
             <tbody>
-              {
-                filteredAccount.map((e, i) => {
+              {displayedData.map((e, i) => {
+                return (
+                  <tr>
+                    <td>{e.id}</td>
+                    <td>{e.table_name}</td>
+                    <td>{e.collected_by}</td>
+                    <td>{e.collected_user}</td>
+                    <td>{e.date}</td>
+                    <td>{e.cat_name}</td>
+                    {e.type == 1 ? (
+                      <>
+                        <td style={{ color: "green" }}>{e.amount}</td>
+                        <td style={{ color: "red" }}>-</td>
+                      </>
+                    ) : (
+                      <>
+                        <td style={{ color: "green" }}>-</td>
+                        <td style={{ color: "red" }}>{e.amount}</td>
+                      </>
+                    )}
+                  </tr>
+                );
+              })}
 
-                  return (
-
-                    <tr >
-                      <td>{e.id}</td>
-                      <td>{e.table_name}</td>
-                      <td>{e.collected_by}</td>
-                      <td>{e.collected_user}</td>
-                      <td>{e.date}</td>
-                      <td>{e.cat_name}</td>
-                      {
-                        e.type == 1 ?
-                          <>
-                            <td style={{ color: "green" }}>{e.amount}</td>
-                            <td style={{ color: "red" }}>-</td>
-                          </>
-                          :
-                          <>
-                            <td style={{ color: "green" }}>-</td>
-                            <td style={{ color: "red" }}>{e.amount}</td>
-                          </>
-                      }
-                    </tr>
-                  )
-                })
-              }
-
-              <tr className="border-t-2 mt-4 border-gray-300" >
+              <tr className="border-t-2 mt-4 border-gray-300">
                 <td></td>
                 <td></td>
                 <td></td>
@@ -206,7 +205,7 @@ const Dashboard = ({ memberId }) => {
                 <td style={{ color: "red" }}>{totalDebit}</td>
               </tr>
 
-              <tr className="border-t-2 mt-4 border-gray-300" >
+              <tr className="border-t-2 mt-4 border-gray-300">
                 <td></td>
                 <td></td>
                 <td></td>
@@ -214,21 +213,25 @@ const Dashboard = ({ memberId }) => {
                 <td></td>
                 <td>Net</td>
 
-                {
-                  (NetProfit) > 0 ?
-                    <>
-                      <td style={{ color: "green" }}>{NetProfit}</td>
-                      <td>-</td>
-                    </> :
-                    <>
-                      <td>-</td>
-                      <td style={{ color: "red" }}>{NetProfit}</td>
-                    </>
-                }
+                {NetProfit > 0 ? (
+                  <>
+                    <td style={{ color: "green" }}>{NetProfit}</td>
+                    <td>-</td>
+                  </>
+                ) : (
+                  <>
+                    <td>-</td>
+                    <td style={{ color: "red" }}>{NetProfit}</td>
+                  </>
+                )}
               </tr>
-
             </tbody>
           </table>
+          <Pagination
+            currentPage={currentPage}
+            totalPages={Math.ceil(filteredAccount.length / itemsPerPage)}
+            onPageChange={handlePageChange}
+          />
 
           {/* <div class="header">
             <i class="bx bx-receipt"></i>
--- pages/api/accounting/[id].js
+++ pages/api/accounting/[id].js
@@ -3,80 +3,84 @@
 import conn from "../dbconfig/conn";
 
 export default async function handler(req, res) {
-    const { id } = req.query; // Get the dynamic ID from the URL parameter
+  const { id } = req.query; // Get the dynamic ID from the URL parameter
 
-    if (req.method === 'GET') {
-        try {
+  if (req.method === "GET") {
+    try {
+      // const sq = "    SELECT 'cf_main_payment' AS table_name, id, amount,collected_by, collected_user, date, note, m_id, c_id FROM cf_main_payment WHERE m_id = 14 UNION ALL SELECT 'cf_main_payment_return' AS table_name, id, amount, return_by, returned_user, date, note, m_id, c_id FROM cf_main_payment_return WHERE m_id = 14 UNION ALL SELECT 'cf_borrow_payment' AS table_name, id, amount, given_by AS collected_by, given_user AS collected_user, date, note, m_id, bail_m_id AS c_id FROM cf_borrow_payment WHERE m_id = 14 UNION ALL SELECT 'cf_deposit_borrowed_payment' AS table_name, id, amount, deposite_by AS collected_by, collected_user, date, note, m_id, NULL AS c_id FROM cf_deposit_borrowed_payment WHERE m_id = 14 ORDER BY date; "
+      console.log("*********THIS*********" + id);
 
-            // const sq = "    SELECT 'cf_main_payment' AS table_name, id, amount,collected_by, collected_user, date, note, m_id, c_id FROM cf_main_payment WHERE m_id = 14 UNION ALL SELECT 'cf_main_payment_return' AS table_name, id, amount, return_by, returned_user, date, note, m_id, c_id FROM cf_main_payment_return WHERE m_id = 14 UNION ALL SELECT 'cf_borrow_payment' AS table_name, id, amount, given_by AS collected_by, given_user AS collected_user, date, note, m_id, bail_m_id AS c_id FROM cf_borrow_payment WHERE m_id = 14 UNION ALL SELECT 'cf_deposit_borrowed_payment' AS table_name, id, amount, deposite_by AS collected_by, collected_user, date, note, m_id, NULL AS c_id FROM cf_deposit_borrowed_payment WHERE m_id = 14 ORDER BY date; "
-            console.log("*********THIS*********" + id);
+      const q =
+        "SELECT main_query.*, category.name AS cat_name FROM ( SELECT 'Add Payment' AS table_name, '1' AS type, id, amount, collected_by, collected_user, DATE_FORMAT(date, '%Y-%m-%d') as date, note, m_id, c_id FROM cf_main_payment WHERE m_id = ? UNION ALL SELECT 'Refund Payment' AS table_name, '0' AS type, id, amount, return_by, returned_user, DATE_FORMAT(date, '%Y-%m-%d') as date, note, m_id, c_id FROM cf_main_payment_return WHERE m_id = ? UNION ALL SELECT 'Borrow Payment' AS table_name, '0' AS type, id, amount, given_by AS collected_by, given_user AS collected_user, DATE_FORMAT(date, '%Y-%m-%d') as date, note, m_id, NULL AS c_id FROM cf_borrow_payment WHERE m_id = ? UNION ALL SELECT 'Deposite Payment' AS table_name, '1' AS type, id, amount, deposite_by AS  collected_by, collected_user, DATE_FORMAT(date, '%Y-%m-%d') as date, note, m_id, NULL AS c_id FROM cf_deposit_borrowed_payment WHERE m_id = ? ) AS main_query LEFT JOIN cf_category AS category ON main_query.c_id = category.id ORDER BY main_query.date;";
 
-
-            const q = "SELECT main_query.*, category.name AS cat_name FROM ( SELECT 'Add Payment' AS table_name, '1' AS type, id, amount, collected_by, collected_user, DATE_FORMAT(date, '%Y-%m-%d') as date, note, m_id, c_id FROM cf_main_payment WHERE m_id = ? UNION ALL SELECT 'Refund Payment' AS table_name, '0' AS type, id, amount, return_by, returned_user, DATE_FORMAT(date, '%Y-%m-%d') as date, note, m_id, c_id FROM cf_main_payment_return WHERE m_id = ? UNION ALL SELECT 'Borrow Payment' AS table_name, '0' AS type, id, amount, given_by AS collected_by, given_user AS collected_user, DATE_FORMAT(date, '%Y-%m-%d') as date, note, m_id, NULL AS c_id FROM cf_borrow_payment WHERE m_id = ? UNION ALL SELECT 'Deposite Payment' AS table_name, '1' AS type, id, amount, deposite_by AS  collected_by, collected_user, DATE_FORMAT(date, '%Y-%m-%d') as date, note, m_id, NULL AS c_id FROM cf_deposit_borrowed_payment WHERE m_id = ? ) AS main_query LEFT JOIN cf_category AS category ON main_query.c_id = category.id ORDER BY main_query.date;";
-
-            const data = [
-                id,
-                id,
-                id,
-                id
-            ]
-            const [rows] = await conn.query(q, data);
-            console.log("******************");
-            console.log(rows);
-            res.status(200).json(rows);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ error: 1, msg: "Borrow Payment Cannot Fetch... Check Connection" });
-        }
+      const data = [id, id, id, id];
+      const [rows] = await conn.query(q, data);
+      console.log("******************");
+      console.log(rows);
+      res.status(200).json(rows);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: 1,
+        msg: "Borrow Payment Cannot Fetch... Check Connection",
+      });
     }
+  }
 
+  if (req.method == "DELETE") {
+    try {
+      const { id } = req.query;
+      console.log(id);
+      // Query the database
+      const q = "DELETE FROM cf_borrow_payment WHERE id = ?";
+      console.log(q);
+      const [rows] = await conn.query(q, [id]);
 
-    if (req.method == 'DELETE') {
-        try {
-            const { id } = req.query
-            console.log(id);
-            // Query the database
-            const q = "DELETE FROM cf_borrow_payment WHERE id = ?"
-            console.log(q);
-            const [rows] = await conn.query(q, [id]);
-
-            // Process the data and send the response
-            res.status(200).json(rows);
-        } catch (error) {
-            if (error.toString().includes("Cannot delete or update a parent row")) {
-                res.status(500).json({ error: 1, msg: "Borrow Payment Not Deleted... Already Use In Any Payment" });
-            }
-            else {
-                res.status(500).json({ error: 1, msg: "Borrow Payment Cannot Delete... Check Connection" })
-            }
-        }
+      // Process the data and send the response
+      res.status(200).json(rows);
+    } catch (error) {
+      if (error.toString().includes("Cannot delete or update a parent row")) {
+        res.status(500).json({
+          error: 1,
+          msg: "Borrow Payment Not Deleted... Already Use In Any Payment",
+        });
+      } else {
+        res.status(500).json({
+          error: 1,
+          msg: "Borrow Payment Cannot Delete... Check Connection",
+        });
+      }
     }
+  }
 
-    if (req.method == 'PATCH') {
-
-        const { amount, collectedby, note, date, mid, bailmid, username } = req.body
-        try {
-            // Query the database
-            const q = "UPDATE `cf_borrow_payment` SET `amount`=?, `date`=?, `note`=?, `m_id`=?, `bail_m_id`=?, `given_by`=?, `given_user`=?  WHERE id = ?"
-            console.log(q);
-            const data = [
-                amount,
-                date,
-                note,
-                mid,
-                bailmid,
-                collectedby,
-                username,
-                id
-            ]
-            const [rows] = await conn.query(q, data);
-            console.log(data);
-            // Process the data and send the response
-            res.status(200).json(rows);
-        } catch (error) {
-            console.error('Error fetching users:', error);
-            res.status(500).json({ error: 1, msg: "Borrow Payment Cannot Update... Check Connection" });
-        }
+  if (req.method == "PATCH") {
+    const { amount, collectedby, note, date, mid, bailmid, username } =
+      req.body;
+    try {
+      // Query the database
+      const q =
+        "UPDATE `cf_borrow_payment` SET `amount`=?, `date`=?, `note`=?, `m_id`=?, `bail_m_id`=?, `given_by`=?, `given_user`=?  WHERE id = ?";
+      console.log(q);
+      const data = [
+        amount,
+        date,
+        note,
+        mid,
+        bailmid,
+        collectedby,
+        username,
+        id,
+      ];
+      const [rows] = await conn.query(q, data);
+      console.log(data);
+      // Process the data and send the response
+      res.status(200).json(rows);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).json({
+        error: 1,
+        msg: "Borrow Payment Cannot Update... Check Connection",
+      });
     }
-
+  }
 }
